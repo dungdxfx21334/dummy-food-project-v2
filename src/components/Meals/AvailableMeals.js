821cfac5983c1230c855221caba3818bd380222f
Dao Xuan Dung <dungdxfx21334@funix.edu.vn>
2023-09-01 16:33:44 +0700

    Migrating dummy meals data to the fire base and then fetch it from there

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,36 +1,25 @@
-import Card from '../UI/Card';
-import MealItem from './MealItem/MealItem';
-import classes from './AvailableMeals.module.css';
-
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
+import Card from '../UI/Card'
+import MealItem from './MealItem/MealItem'
+import classes from './AvailableMeals.module.css'
+import { useEffect, useState, useCallback } from 'react'
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([])
+
+  const fetchingMeals = useCallback(async () => {
+    const response = await fetch(
+      'https://meal-http-4df27-default-rtdb.asia-southeast1.firebasedatabase.app/DUMMY_MEALS.json'
+    )
+    const data = await response.json()
+
+    setMeals(Object.values(data)[0].meals)
+  }, [])
+
+  useEffect(() => {
+    fetchingMeals()
+  }, [fetchingMeals])
+
+  const mealsList = meals.map(meal => (
     <MealItem
       key={meal.id}
       id={meal.id}
@@ -38,7 +27,7 @@ const AvailableMeals = () => {
       description={meal.description}
       price={meal.price}
     />
-  ));
+  ))
 
   return (
     <section className={classes.meals}>
@@ -46,7 +35,7 @@ const AvailableMeals = () => {
         <ul>{mealsList}</ul>
       </Card>
     </section>
-  );
-};
+  )
+}
 
-export default AvailableMeals;
+export default AvailableMeals
