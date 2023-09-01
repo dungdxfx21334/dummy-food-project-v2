c85a0b33331b9a5d74e75877ea6fa087f010e8f8
Dao Xuan Dung <dungdxfx21334@funix.edu.vn>
2023-09-01 21:50:01 +0700

    Adding Checkout component

---
+++ src/components/Cart/Checkout.js
@@ -0,0 +1,28 @@
+import classes from './Checkout.module.css'
+const Checkout = props => {
+  return (
+    <form>
+      <div className={classes.control}>
+        <label htmlFor='name'>Your Name</label>
+        <input id='name' type='text'></input>
+      </div>
+      <div className={classes.control}>
+        <label htmlFor='street'>Street</label>
+        <input id='street' type='text'></input>
+      </div>
+      <div className={classes.control}>
+        <label htmlFor='postal'>Postal Code</label>
+        <input id='postal' type='text'></input>
+      </div>
+      <div className={classes.control}>
+        <label htmlFor='city'>City</label>
+        <input id='city' type='text'></input>
+      </div>
+      <button type='button' onClick={props.onCancel}>
+        Cancel
+      </button>
+      <button>Confirm</button>
+    </form>
+  )
+}
+export default Checkout
