c9e5232a1a1c6d51f9ff7f6f89c76fa89a56e183
Dao Xuan Dung <dungdxfx21334@funix.edu.vn>
2023-09-01 21:52:17 +0700

    Update Checkout component's structure

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,28 +1,36 @@
 import classes from './Checkout.module.css'
+
 const Checkout = props => {
+  const confirmHandler = event => {
+    event.preventDefault()
+  }
+
   return (
-    <form>
+    <form className={classes.form} onSubmit={confirmHandler}>
       <div className={classes.control}>
         <label htmlFor='name'>Your Name</label>
-        <input id='name' type='text'></input>
+        <input type='text' id='name' />
       </div>
       <div className={classes.control}>
         <label htmlFor='street'>Street</label>
-        <input id='street' type='text'></input>
+        <input type='text' id='street' />
       </div>
       <div className={classes.control}>
         <label htmlFor='postal'>Postal Code</label>
-        <input id='postal' type='text'></input>
+        <input type='text' id='postal' />
       </div>
       <div className={classes.control}>
         <label htmlFor='city'>City</label>
-        <input id='city' type='text'></input>
+        <input type='text' id='city' />
       </div>
-      <button type='button' onClick={props.onCancel}>
-        Cancel
-      </button>
-      <button>Confirm</button>
+      <div className={classes.actions}>
+        <button type='button' onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button className={classes.submit}>Confirm</button>
+      </div>
     </form>
   )
 }
+
 export default Checkout
